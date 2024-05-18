f2a01dcac71ce2a87cb0d3120d14e9ebca15d642
ductd10062003 <ductdhe176150@fpt.edu.vn>
2024-05-18 22:57:53 +0700

    Base code

--- SE1847_SWP391_G1/src/java/entity/User.java
+++ SE1847_SWP391_G1/src/java/entity/User.java
@@ -3,18 +3,26 @@ package entity;
 public class User {
     private int user_id;
     private String name, email, password;
-    private int role, status;
-
+    private int role, active;    
+    private String create_at;
+    private int gender;
+    public String dob, phone, image;
+    
     public User() {
     }
 
-    public User(int user_id, String name, String email, String password, int role, int status) {
+    public User(int user_id, String name, String email, String password, int role, int active, String create_at, int gender, String dob, String phone, String image) {
         this.user_id = user_id;
         this.name = name;
         this.email = email;
         this.password = password;
         this.role = role;
-        this.status = status;
+        this.active = active;
+        this.create_at = create_at;
+        this.gender = gender;
+        this.dob = dob;
+        this.phone = phone;
+        this.image = image;
     }
 
     public int getUser_id() {
@@ -57,18 +65,57 @@ public class User {
         this.role = role;
     }
 
-    public int getStatus() {
-        return status;
+    public int getActive() {
+        return active;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(String create_at) {
+        this.create_at = create_at;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     @Override
     public String toString() {
-        return "User{" + "user_id=" + user_id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role + ", status=" + status + '}';
+        return "User{" + "user_id=" + user_id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role + ", active=" + active + ", create_at=" + create_at + ", gender=" + gender + ", dob=" + dob + ", phone=" + phone + ", image=" + image + '}';
     }
-    
-    
+
 }
--- SE1847_SWP391_G1/src/java/model/DAOUser.java
+++ SE1847_SWP391_G1/src/java/model/DAOUser.java
@@ -7,36 +7,6 @@ import java.sql.ResultSet;
 
 public class DAOUser extends DBConnect {
 
-    public Vector<User> getAllUsers() {
-        Vector<User> vectorUser = new Vector<>();
-        String sql = "select * from [user]";
-        try {
-            PreparedStatement ps = conn.prepareCall(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                vectorUser.add(new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6)));
-            }
-        } catch (Exception e) {
-            return null;
-        }
-        return vectorUser;
-    }
 
-    public User getUser(String email, String password) {
-        User user = new User();
-        String sql = "select * from [user] where [email]=? and [password]=?";
-        try {
-            PreparedStatement ps = conn.prepareCall(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ps.setString(1, email);
-            ps.setString(2, password);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                user = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6));
-            }
-        } catch (Exception e) {
-            return null;
-        }
-        return user;
-    }
 
 }
