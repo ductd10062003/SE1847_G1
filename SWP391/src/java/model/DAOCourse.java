768d87ec07ee1452637602c13abb78f6733d9750
ductd10062003 <ductdhe176150@fpt.edu.vn>
2024-05-22 09:53:29 +0700

    22/5/2024

---
+++ SWP391/src/java/model/DAOCourse.java
@@ -0,0 +1,39 @@
+package model;
+
+import entity.Course;
+import java.util.Vector;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DAOCourse extends DBConnect {
+    public Vector<Course> getAllCourses(){
+        Vector<Course> vector = new Vector<>();
+        String sql = "select * from [course]";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                vector.add(new Course(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getInt(8)));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return vector;
+    }
+    
+    public Course getCourseByID(int course_id){
+        Course course = new Course();
+        String sql = "select * from [course] where course_id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ps.setInt(1, course_id);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                course = new Course(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getInt(8));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return course;
+    }
+}
---
+++ SWP391/src/java/model/DAOFlashCard.java
@@ -0,0 +1,59 @@
+package model;
+
+import entity.FlashCard;
+import entity.Quiz;
+import java.util.Vector;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DAOFlashCard extends DBConnect {
+
+    public Vector<FlashCard> getAllFlashCards() {
+        String sql = "select * from flashcard";
+        Vector<FlashCard> vector = new Vector<>();
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                vector.add(new FlashCard(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getString(8)));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return vector;
+    }
+
+    public FlashCard getFlashCardByID(int flashcard_id) {
+        String sql = "select * from flashcard where flashcard_id = ?";
+        FlashCard flashcard = new FlashCard();
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                flashcard = new FlashCard(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getString(8));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return flashcard;
+    }
+
+    public Vector<FlashCard> getFlashCardByCourse(Vector<Quiz> quizzes) {
+        Vector<FlashCard> vector = new Vector<>();
+        for (Quiz quiz : quizzes) {
+            try {
+                String sql = "select * from flashcard where flashcard_id = ?";
+                PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                ps.setInt(1, quiz.getFlashcard_id());
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    vector.add(new FlashCard(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getString(8)));
+                }
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return vector;
+    }
+
+}
